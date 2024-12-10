1a97c7ba35f044cd099ede85fa4700f98e0868bb
Jonathan Szkup <jszkup101@gmail.com>
2024-12-10 18:22:29 -0500

    Board is now Cell objects instead of zeroes, Player is now a factory function, start work on a working full round

--- scripts/script.js
+++ scripts/script.js
@@ -2,16 +2,17 @@
 // *Board for the game - WIll be an IIFE
 (function gameBoard() {
     // create a 3 x 3 array to be used as a game board
-    // TODO should i fill these with objects so that I can assign a player to each move
     let board = [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]
+        [Cell(), Cell(), Cell()],
+        [Cell(), Cell(), Cell()],
+        [Cell(), Cell(), Cell()]
     ];
 
     const getBoard = () => board;
 
     // TODO function to find available spots in the board
+    // TODO players will be an object of name & X/O
+    // TODO check if there isnt a 0(Cell.getValue != 0), if there is place the players token (filter & map)
 
     const printBoard = () => {
         console.log(board);
@@ -23,15 +24,57 @@
     }
 })();
 
+function Cell() {
+    let value = 0;
+
+    // TODO needs to work with new player object getPlayerToken
+    const addToken = (player) => {
+        value = player;
+    };
+
+    const getValue = () => value;
+
+    return {
+        addToken,
+        getValue
+    };
+}
+// player object
+// record player score, increase it - player name
+function createPlayer(name, token) {
+    let score = 0;
+    let token = token;
+
+    const getPlayerName = () => name;
+
+    const getPlayerToken = () => token;
+
+    const getScore = () => score;
+    const increaseScore = () => score++;
+
+    return {
+        getPlayerName,
+        getScore,
+        increaseScore,
+        getPlayerToken
+    }
+}
+
 
 // game object 
 // *Game rules are defined and played out
-function gameController(playerOneName = "Jon", playerTwoName = "CPU") {
+function gameController(playerOne = createPlayer("Jon", "X"), playerTwo = createPlayer("CPU", "O")) {
     const board = gameBoard();
 
     const players = [
-        createPlayer(playerOneName),
-        createPlayer(playerTwoName)
+        {
+            name: playerOne.getPlayerName,
+            token: playerOne.getPlayerToken
+        },
+        {
+            name: playerTwo.getPlayerName,
+            token: playerTwo.getPlayerToken
+        }
     ];
 
     let activePlayer = players[0];
@@ -46,23 +89,20 @@ function gameController(playerOneName = "Jon", playerTwoName = "CPU") {
         console.log(`${getActivePlayer().getPlayerName}'s turn.`);
     }
 
-}
-// if a space has already been filled in you cant overwrite
+    //  TODO should I pass "column" into this?
+    const playRound = () => {
+        // TODO print the current players turn to console
+        // TODO place X/O in an available space
 
-// player object
-// #record player score, increase it - player name
-function createPlayer(name) {
-    let score = 0;
-
-    const getPlayerName = () => name;
-
-    // TODO instead of tracking just score I need to track if a player is an X or an O
-    const getScore = () => score;
-    const increaseScore = () => score++;
+        switchPlayerTurn();
+        printNewRound();
+    }
 
     return {
-        getPlayerName,
-        getScore,
-        increaseScore
+        getActivePlayer,
+        getBoard: board.getBoard,
+        // playRound
     }
+
 }
+// if a space has already been filled in you cant overwrite
